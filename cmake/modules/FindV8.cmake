4fc9fb587876e1ee90a472bef9f13527108600c9
jsm174 <jsm174@gmail.com>
2021-08-24 18:40:59 -0400

    v8: plumb v8 into cmake

---
+++ cmake/modules/FindV8.cmake
@@ -0,0 +1,96 @@
+# Courtesy of: https://raw.githubusercontent.com/gwaldron/osgearth/master/CMakeModules/FindV8.cmake
+#
+# Locate V8
+# This module defines
+# V8_LIBRARY
+# V8_FOUND, if false, do not try to link to V8
+# V8_INCLUDE_DIR, where to find the headers
+
+IF (NOT $ENV{V8_DIR} STREQUAL "")
+  SET(V8_DIR $ENV{V8_DIR})
+ENDIF()
+
+
+SET(V8_LIBRARY_SEARCH_PATHS
+  ${V8_DIR}/
+  ${V8_DIR}/lib/
+  ${V8_DIR}/build/Release/lib/
+  ${V8_DIR}/build/Release/lib/third_party/icu/
+  ${V8_DIR}/build/Release/obj/
+  ${V8_DIR}/build/Release/obj/third_party/icu/
+  ${V8_DIR}/out/ia32.release/lib.target/
+  ${V8_DIR}/out/ia32.release/lib.target/third_party/icu/
+  ${V8_DIR}/out/ia32.release/obj/
+  ${V8_DIR}/out/ia32.release/obj/third_party/icu/
+  ${V8_DIR}/out.gn/ia32.release/lib.target/
+  ${V8_DIR}/out.gn/ia32.release/lib.target/third_party/icu/
+  ${V8_DIR}/out.gn/ia32.release/obj/
+  ${V8_DIR}/out.gn/ia32.release/obj/third_party/icu/
+  ${V8_DIR}/out/x64.release/lib.target/
+  ${V8_DIR}/out/x64.release/lib.target/third_party/icu/
+  ${V8_DIR}/out/x64.release/obj/
+  ${V8_DIR}/out/x64.release/obj/third_party/icu/
+  ${V8_DIR}/out.gn/x64.release/lib.target/
+  ${V8_DIR}/out.gn/x64.release/lib.target/third_party/icu/
+  ${V8_DIR}/out.gn/x64.release/obj/
+  ${V8_DIR}/out.gn/x64.release/obj/third_party/icu/
+  ~/Library/Frameworks
+  /Library/Frameworks
+  /usr/local/lib
+  /usr/lib
+  /sw/lib
+  /opt/local/lib
+  /opt/csw/lib
+  /opt/lib
+  /usr/freeware/lib64
+)
+
+FIND_PATH(V8_INCLUDE_DIR v8.h
+  ${V8_DIR}
+  ${V8_DIR}/include
+  ~/Library/Frameworks
+  /Library/Frameworks
+  /usr/local/include
+  /usr/include
+  /sw/include # Fink
+  /opt/local/include # DarwinPorts
+  /opt/csw/include # Blastwave
+  /opt/include
+  /usr/freeware/include
+  /devel
+)
+
+FIND_LIBRARY(V8_BASE_LIBRARY
+  NAMES libv8_base.a v8_base.a v8 v8_base libv8_base
+  PATHS ${V8_LIBRARY_SEARCH_PATHS}
+)
+
+FIND_LIBRARY(V8_LIBPLATFORM_LIBRARY
+  NAMES v8_libplatform libv8_libplatform
+  PATHS ${V8_LIBRARY_SEARCH_PATHS}
+)
+
+FIND_LIBRARY(V8_LIBBASE_LIBRARY
+  NAMES v8_libbase libv8_libbase
+  PATHS ${V8_LIBRARY_SEARCH_PATHS}
+)
+
+FIND_LIBRARY(V8_SNAPSHOT_LIBRARY
+  NAMES v8_snapshot libv8_snapshot
+  PATHS ${V8_LIBRARY_SEARCH_PATHS}
+)
+
+FIND_LIBRARY(V8_LIBSAMPLER_LIBRARY
+  NAMES v8_libsampler libv8_libsampler
+  PATHS ${V8_LIBRARY_SEARCH_PATHS}
+)
+
+SET(V8_FOUND "NO")
+
+IF (NOT UNIX)
+  IF (V8_BASE_LIBRARY AND V8_SNAPSHOT_LIBRARY AND V8_ICUUC_LIBRARY AND V8_ICUI18N_LIBRARY AND V8_INCLUDE_DIR)
+    SET(V8_FOUND "YES")
+  ENDIF()
+ELSEIF(V8_LIBRARY AND V8_ICUUC_LIBRARY AND V8_ICUI18N_LIBRARY AND V8_INCLUDE_DIR)
+  SET(V8_FOUND "YES")
+ENDIF(NOT UNIX)
--- src/main.cpp
+++ src/main.cpp
@@ -23,10 +23,60 @@
 #include "Trigger.h"
 #include "HitTarget.h"
 
+#include "libplatform/libplatform.h"
+#include "v8.h"
+
 std::map<ItemTypeEnum, EditableInfo> EditableRegistry::m_map;
 
+void v8_test() {
+  v8::V8::InitializeICUDefaultLocation(".");
+  v8::V8::InitializeExternalStartupData(".");
+  std::unique_ptr<v8::Platform> platform = v8::platform::NewDefaultPlatform();
+  v8::V8::InitializePlatform(platform.get());
+  v8::V8::Initialize();
+
+  v8::Isolate::CreateParams create_params;
+  create_params.array_buffer_allocator =
+      v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+
+  v8::Isolate* isolate = v8::Isolate::New(create_params);
+  {
+    v8::Isolate::Scope isolate_scope(isolate);
+
+    // Create a stack-allocated handle scope.
+    v8::HandleScope handle_scope(isolate);
+
+    // Create a new context.
+    v8::Local<v8::Context> context = v8::Context::New(isolate);
+
+    // Enter the context for compiling and running the hello world script.
+    v8::Context::Scope context_scope(context);
+
+    {
+      // Create a string containing the JavaScript source code.
+      v8::Local<v8::String> source =
+          v8::String::NewFromUtf8Literal(isolate, "'Hello' + ', World!'");
+
+      // Compile the source code.
+      v8::Local<v8::Script> script =
+          v8::Script::Compile(context, source).ToLocalChecked();
+
+      // Run the script to get the result.
+      v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
+
+      // Convert the result to an UTF8 string and print it.
+      v8::String::Utf8Value utf8(isolate, result);
+      printf("%s\n", *utf8);
+    }
+  }
+
+  // Dispose the isolate and tear down V8.
+  isolate->Dispose();
+  v8::V8::Dispose();
+  v8::V8::ShutdownPlatform();
+}
+
 int main(int argc, char ** argv) {
-    
     EditableRegistry::RegisterEditable<Bumper>();
     EditableRegistry::RegisterEditable<Decal>();
     EditableRegistry::RegisterEditable<DispReel>();
@@ -58,6 +108,8 @@ int main(int argc, char ** argv) {
     }
 
     pPinTable->LoadGameFromFilename(filename.c_str());
+
+    v8_test();
 
     printf("done");
 }
