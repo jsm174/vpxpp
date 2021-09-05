a287a48c2a7ffbaef487c89502a5e81188485f8f
jsm174 <jsm174@gmail.com>
2021-09-05 17:21:57 -0400

    vulkan: attempt to install vulkan sdk and integrate into cmake

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,9 @@
 #include "libplatform/libplatform.h"
 #include "v8.h"
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 std::map<ItemTypeEnum, EditableInfo> EditableRegistry::m_map;
 
 void v8_test()
@@ -77,6 +80,70 @@ void v8_test()
 	v8::V8::ShutdownPlatform();
 }
 
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+}
+
+void glfw_test()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(1024, 768, "vpxpp", NULL, NULL);
+
+	uint32_t count;
+	const char** extensions = glfwGetRequiredInstanceExtensions(&count);
+
+	VkApplicationInfo appInfo{};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+
+	VkInstanceCreateInfo instanceCreateInfo = {};
+	instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	instanceCreateInfo.pApplicationInfo = &appInfo;
+	instanceCreateInfo.enabledExtensionCount = count;
+	instanceCreateInfo.ppEnabledExtensionNames = extensions;
+
+	VkInstance instance;
+	if (vkCreateInstance(&instanceCreateInfo, NULL, &instance) != VK_SUCCESS)
+	{
+		exit(EXIT_FAILURE);
+	}
+
+	uint32_t deviceCount = 0;
+	vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+	std::vector<VkPhysicalDevice> devices(deviceCount);
+	vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+	VkPhysicalDevice physicalDevice = devices[0];
+
+	glfwSetKeyCallback(window, key_callback);
+
+	int i = 0;
+
+	while (!glfwWindowShouldClose(window) && i++ < 100000)
+	{
+		glfwPollEvents();
+	}
+
+	VkSurfaceKHR surface;
+	VkResult err = glfwCreateWindowSurface(instance, window, NULL, &surface);
+
+	if (err)
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	glfwTerminate();
+}
+
 int main(int argc, char** argv)
 {
 	EditableRegistry::RegisterEditable<Bumper>();
@@ -113,6 +180,8 @@ int main(int argc, char** argv)
 	pPinTable->LoadGameFromFilename(filename.c_str());
 
 	v8_test();
+
+	glfw_test();
 
 	printf("done");
 }
